5ac48d3f8ded22ffce0fa453174b024161ec5561
vishalkore22 <vishalkore@gmail.com>
2026-05-17 13:56:38 +0530

    Add files via upload

---
+++ ManufacturingERP/Areas/User/Views/Models/MLedgerAddressViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Manufacturing_Core.Entity;
+
+namespace ManufacturingERP.Areas.User.Models
+{
+    public class MLedgerAddressViewModel
+    {
+        public MLedger mLedgers { get; set; }
+        public MAddress mAddresses { get; set; }        
+    }
+}
---
+++ ManufacturingERP/Areas/User/Views/Models/MSupplierViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Manufacturing_Core.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManufacturingERP.Areas.User.Models
+{
+    public class MSupplierViewModel
+    {       
+        public MSupplier? MSupplier { get; set; }
+        public MAddress? MAddress { get; set; }
+    }
+}
